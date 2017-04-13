3c426a5fa713013bbcc31c785df38d193b8f462d
BoraALAP <alapbora@gmail.com>
2017-04-13 12:15:15 -0400

    improved Gulp, still needs js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const watchSass = require("gulp-watch-sass")
+const watchSass = require("gulp-watch-sass");
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
 
 gulp.task('sass', function() {
-  gulp.src('scss/style.scss')
-  .pipe(sass({style: 'expanded'}))
-  
+  gulp.src('scss/**/*.scss')
+  	.pipe(sourcemaps.init())
+  	.pipe(sass({style: 'expanded'}))
+  	
+  	.pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+  	
+    .pipe(sourcemaps.write())
+    .pipe(concat('styles.scss'))
   .pipe(gulp.dest('css'))
 });
 
 gulp.task("sass:watch", () => {
-  gulp.watch("scss/*.scss", ["sass"])
+  gulp.watch("scss/*.scss", ["sass"]);
+
 })
+
