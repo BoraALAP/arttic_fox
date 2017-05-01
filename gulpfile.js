357caeca89ae8b6976045992d723c6bc89be7099
BoraALAP <alapbora@gmail.com>
2017-04-30 20:36:53 -0400

    changed alot of gulp stuff

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,27 @@ const watchSass = require("gulp-watch-sass");
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
+const livereload = require('gulp-livereload');
 
 gulp.task('sass', function() {
   gulp.src('scss/**/*.scss')
   	.pipe(sourcemaps.init())
-  	.pipe(sass({style: 'expanded'}))
-  	
+  	.pipe(sass({style: 'compressed'}))
+  	.pipe(cleanCSS({compatibility: 'ie8'}))
   	.pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
-        }))
-  	
+        }))  	
     .pipe(sourcemaps.write())
-    .pipe(concat('styles.scss'))
-  .pipe(gulp.dest('css'))
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('css'))
+    .pipe(livereload());
 });
 
-gulp.task("sass:watch", () => {
-  gulp.watch("scss/*.scss", ["sass"]);
+gulp.task("watch", () => {
+  livereload({ start: true });
+  gulp.watch("scss/**/*.scss", ["sass"]);
 
 })
 
