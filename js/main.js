c6f1cb35b1dc5ce558e88774e2536424e192a0af
BoraALAP <alapbora@gmail.com>
2017-05-08 02:31:21 -0400

    Structer and little bit of animation work is done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 jQuery(document).ready(function($){
-	
+	$('#fullpage').fullpage({
+		autoScrolling: false,
+		fitToSection: false,
+		navigation: true,
+		navigationPosition: 'left',
+		showActiveTooltip: false
 
+	});
+
+	$( window ).scroll( () => {
+		if(window.pageYOffset > 20){
+			$('#logo').addClass('play');
+		} else {
+			$('#logo').removeClass('play');
+		}
+	});
 })
 
 
